6399c32ec00b8ea66b186df6b557a6b169807611
Anna <kokshiro@gmail.com>
2024-03-29 23:12:17 +1000

    created tests

--- category_and_product.py
+++ category_and_product.py
@@ -4,10 +4,15 @@ class Category:
     description = str
     product = list
 
+    total_categories = 0
+    total_unique_products = 0
+
     def __init__(self, name, description, product):
         self.name = name
         self.description = description
         self.product = product
+        Category.total_categories += 1
+        Category.total_unique_products += len(product)
 
 class Product:
     name = str
