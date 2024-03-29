26e72cb6fe9314625428b1e59787e5bbd972d4cc
Anna <kokshiro@gmail.com>
2024-03-29 22:00:39 +1000

    add class product and category

---
+++ category_and_product.py
@@ -0,0 +1,24 @@
+class Category:
+
+    name = str
+    description = str
+    product = list
+
+    def __init__(self, name, description, product):
+        self.name = name
+        self.description = description
+        self.product = product
+
+class Product:
+    name = str
+    description = str
+    price = float
+    count_in_stock = int
+
+    def __init__(self, name, description, price, count_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.count_in_stock = count_in_stock
+
+
