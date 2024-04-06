88c4027a1c96bd8be9a8d68391286bdc2a54c17c
Anna <kokshiro@gmail.com>
2024-04-06 22:42:58 +1000

    added new feature

--- category_and_product.py
+++ category_and_product.py
@@ -7,14 +7,25 @@ class Category:
     total_categories = 0
     total_unique_products = 0
 
-    def __init__(self, name, description, product):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-
-        self.product = product
+        self.__products = set()
         Category.total_categories += 1
-        unique_products = set(product)
-        Category.total_unique_products += len(unique_products)
+    def add_product(self, product):
+        self.__products.add(product)
+
+        Category.total_unique_products = len(self.__products)
+
+    @property
+    def products(self):
+        product_info = []
+        for product in self.__products:
+            info = f'{product.name}, {product.price} руб. Остаток: {product.count_in_stock} шт.'
+            product_info.append(info)
+        return "\n".join(product_info)
+
+
 
 
 class Product:
@@ -26,7 +37,37 @@ class Product:
     def __init__(self, name, description, price, count_in_stock):
         self.name = name
         self.description = description
-        self.price = price
-        self.count_in_stock = count_in_stock
+        self.__price = float(price)
+        self.count_in_stock = int(count_in_stock)
 
+    @classmethod
+    def create_product(cls, name, description, price, count_in_stock):
+        return cls(name, description, price, count_in_stock)
 
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена некорректно")
+        else:
+            self.__price = value
+
+one_1 = Category("Electronics", "Electronic devices")
+two_2 = Product("Laptop", "High-performance laptop", 1500, 4)
+two_3 = Product("Mouse", "Wireless mouse", 30.00, 20)
+two_4 = Product("Laptop", "High-performance laptop", 1500, 4)
+one_1.add_product(two_2)
+one_1.add_product(two_3)
+one_1.add_product(two_4)
+
+print(one_1.products)
+
+# Проверка сеттера price
+two_2.price = 1700.0  # Изменение цены на корректное значение
+print(f"Новая цена для {two_2.name}: {two_2.price}")
+
+two_2.price = -5.0  # Попытка установить некорректное значение цены
+print(f"Новая цена для {two_2.name}: {two_2.price}")
