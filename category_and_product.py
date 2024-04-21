90a817f635b8878a2deba20107d74003fdc0661d
Anna <kokshiro@gmail.com>
2024-04-21 21:09:14 +1000

    added method add in class Smartphone and Lawngrass

--- category_and_product.py
+++ category_and_product.py
@@ -69,7 +69,7 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.count_in_stock} шт."
 
     def __add__(self, other):
-        if isinstance(other, type(self)):
+        if isinstance(other, Product):
             return self.price * self.count_in_stock + other.price * other.count_in_stock
         else:
             raise TypeError("Unsupported operand type for +")
@@ -82,6 +82,11 @@ class Smartphone(Product):
         self.model = model
         self.memory = memory
         self.color = color
+    def __add__(self, other):
+        if isinstance(other, Smartphone):
+            return self.price * self.count_in_stock + other.price * other.count_in_stock
+        else:
+            raise TypeError("Unsupported operand type for +")
 
 class LawnGrass(Product):
     def __init__(self, name, description, price, count_in_stock, manufacturer, germination_time, color):
@@ -90,6 +95,11 @@ class LawnGrass(Product):
         self.germination_time = germination_time
         self.color = color
 
+    def __add__(self, other):
+        if isinstance(other, LawnGrass):
+            return self.price * self.count_in_stock + other.price * other.count_in_stock
+        else:
+            raise TypeError("Unsupported operand type for +")
 
 
 # Создание продуктов
